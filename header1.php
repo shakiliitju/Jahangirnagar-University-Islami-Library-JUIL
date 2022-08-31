e9553f081106255771221d7160bb2dd6ee204bd2
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-08-31 17:41:05 +0600

    frontend design

---
+++ header1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>জাবিইপা</title>
+    <link rel="shortcut icon" href="../library/image/juil.ico" type="image/x-icon">
+
+    <!-- <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <!-- font awesome cdn link  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- custom css file link  
+    <link rel="stylesheet" href="css/style.css"> -->
+
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+
+</head>
+
+<body>
+
+    <!-- header section starts  -->
+    <header>
+
+    </header>
+    <!-- header section ends -->
---
+++ login.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if ($_SESSION['login'] != '') {
+    $_SESSION['login'] = '';
+}
+if (isset($_POST['login'])) {
+
+    $email = $_POST['emailid'];
+    $password = md5($_POST['password']);
+    $sql = "SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+    if ($query->rowCount() > 0) {
+        foreach ($results as $result) {
+            $_SESSION['stdid'] = $result->StudentId;
+            if ($result->Status == 1) {
+                $_SESSION['login'] = $_POST['emailid'];
+                echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+            } else {
+                echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
+            }
+        }
+    } else {
+        echo "<script>alert('Invalid Details');</script>";
+    }
+}
+
+?>
+
+<?php include('header1.php'); ?>
+
+<body>
+    <!------MENU SECTION START-->
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
+
+
+    <div class="content-wrapper">
+        <div class="container">
+
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">USER LOGIN FORM</h4>
+                </div>
+            </div>
+            <!-- <a name="ulogin"></a> -->
+            <!--LOGIN PANEL START-->
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            LOGIN FORM
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post">
+
+                                <div class="form-group">
+                                    <label>Enter Email id</label>
+                                    <input class="form-control" type="text" name="emailid" required autocomplete="off" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="form-control" type="password" name="password" required autocomplete="off" />
+                                    <p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
+                                </div>
+
+
+
+                                <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!---LOGIN PABNEL END-->
+
+
+        </div>
+    </div>
+</body>
+
+</html>
