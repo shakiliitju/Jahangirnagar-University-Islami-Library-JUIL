0009c74ac2233b4baa729995d500f4bfd4247e13
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-09-06 14:25:00 +0600

    Team Update

--- js/script.js
+++ js/script.js
@@ -121,30 +121,6 @@ var swiper = new Swiper(".reviews-slider", {
   },
 });
 
-var swiper = new Swiper(".blogs-slider", {
-  spaceBetween: 10,
-  grabCursor: true,
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-
-
 
 
 let section_counter = document.querySelector('#section_counter');
