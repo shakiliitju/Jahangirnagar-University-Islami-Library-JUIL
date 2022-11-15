d1dd9a4e210aecabdbf1c4ec42cf3df761231a9d
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-11-15 07:00:19 +0600

    update

--- header.php
+++ header.php
@@ -1,56 +1,101 @@
 <!-- header section starts  -->
 
 <header class="header">
-        <div class="header-1">
-            <div class="logo">
-                <a href="#">
-                    <img src="image/juil-logo.png" alt="logo">
-                </a>
-            </div>
-
-            <form action="" class="search-form">
-                <input type="search" name="" placeholder="আপনার পছন্দের বই খুঁজুন..." id="search-box">
-                <label for="search-box" class="fas fa-search"></label>
-            </form>
-
-            <div class="icons">
-                <div id="search-btn" class="fas fa-search"></div>
-                <a href="https://www.facebook.com/juislamiclibrary/" target="_blank"><i class="fab fa-facebook"></i></a>
-                <a href="https://www.facebook.com/groups/395317002159903/" target="_blank"><i class="fab fa-facebook"></i></a>
-            </div>
+    <div class="header-1">
+        <div class="logo">
+            <a href="#">
+                <img src="image/juil-logo.png" alt="logo">
+            </a>
         </div>
 
+        <form action="" class="search-form">
+            <input type="search" name="" placeholder="আপনার পছন্দের বই খুঁজুন..." id="search-box">
+            <label for="search-box" class="fas fa-search"></label>
+        </form>
 
-
-        <div class="header-2">
-
-            <nav class="navbar">
-                <a href="index.php">হোম</a>
-                <a href="index.php#featured">বইসমূহ</a>
-                <a href="index.php#arrivals">নতুন বই</a>
-                <a href="index.php#reviews">আমাদের সম্পর্কে</a>
-                <a href="answer.php">প্রশ্ন ও উত্তর</a>
-                <a href="registration.php">নিবন্ধন</a>
-                <a href="contact.php">যোগাযোগ</a>
-                <a href="login.php">লগইন</a>
-            </nav>
+        <div class="icons">
+            <div id="search-btn" class="fas fa-search"></div>
+            <a href="https://www.facebook.com/juislamiclibrary/" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.facebook.com/groups/395317002159903/" target="_blank"><i class="fab fa-facebook"></i></a>
         </div>
+    </div>
 
-    </header>
 
-    
-    <!-- bottom navbar starts -->
-    
-    <nav class="bottom-navbar">
-        <a href="index.php" class="fas fa-home"></a>
-        <!-- <a href="#featured" class="fas fa-list"></a> -->
-        <!-- <a href="#arrivals" class="fas fa-tags"></a> -->
-        <!-- <a href="#reviews" class="fas fa-comments"></a> -->
-        <a href="answer.php" class="fa-sharp fa-solid fa-question"></a>
-        <a href="contact.php" class="fas fa-envelope"></a>
-        <a href="registration.php" class="fa-solid fa-user-plus"></a>
-        <a href="login.php" class="fas fa-user"></a>
-    </nav>
-    
-    <!-- bottom navbar  ends-->
-    <!-- header section ends -->
+
+    <style>
+        .ticker {
+            display: flex;
+            flex-wrap: wrap;
+            height: 20px;
+            margin: 0 auto;
+        }
+
+        .news {
+            width: 80%;
+            background-color: lightblue;
+            padding: 5px 1%;
+        }
+
+        .title {
+            width: 20%;
+            text-align: center;
+            background: #c81c1c;
+            position: relative;
+        }
+
+        .title h5 {
+            font-size: 15px;
+            padding: 5px;
+        }
+
+        .news marquee {
+            font-size: 15px;
+        }
+    </style>
+
+    <div class="ticker">
+        <div class="title">
+            <h5>বিজ্ঞপ্তি :</h5>
+        </div>
+        <div class="news">
+            <marquee>
+                <p>
+                    দাওয়াহ'ই মূল উদ্দেশ্য। পাঠাগার থেকে আপনার প্রয়োজনীয় বই বিনামূল্যে নিয়ে পড়তে পারবেন। আর চাইলে আপনার পছন্দের প্রয়োজনীয় বইটি পাঠাগারে উপহার দিতে পারবেন।
+                </p>
+            </marquee>
+        </div>
+    </div>
+
+
+    <div class="header-2">
+
+        <nav class="navbar">
+            <a href="index.php">হোম</a>
+            <a href="index.php#featured">বইসমূহ</a>
+            <a href="index.php#arrivals">নতুন বই</a>
+            <a href="index.php#reviews">আমাদের সম্পর্কে</a>
+            <a href="answer.php">প্রশ্ন ও উত্তর</a>
+            <a href="registration.php">নিবন্ধন</a>
+            <a href="contact.php">যোগাযোগ</a>
+            <a href="login.php">লগইন</a>
+        </nav>
+    </div>
+
+</header>
+
+
+<!-- bottom navbar starts -->
+
+<nav class="bottom-navbar">
+    <a href="index.php" class="fas fa-home"></a>
+    <!-- <a href="#featured" class="fas fa-list"></a> -->
+    <!-- <a href="#arrivals" class="fas fa-tags"></a> -->
+    <!-- <a href="#reviews" class="fas fa-comments"></a> -->
+    <a href="answer.php" class="fa-sharp fa-solid fa-question"></a>
+    <a href="contact.php" class="fas fa-envelope"></a>
+    <a href="registration.php" class="fa-solid fa-user-plus"></a>
+    <a href="login.php" class="fas fa-user"></a>
+</nav>
+
+<!-- bottom navbar  ends-->
+<!-- header section ends -->
